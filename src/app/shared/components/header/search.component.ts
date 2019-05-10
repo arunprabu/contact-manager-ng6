a1dbd878762bd1679a5cc7bd96734dc44952c8ee
Arun Vijayarengan <arunprabu.v@hotmail.com>
2019-05-11 00:44:48 +0530

    Ref in Form field

--- src/app/shared/components/header/search.component.ts
+++ src/app/shared/components/header/search.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search',
   template: `
-    <p>
-      search works!
-    </p>
+  <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" #searchInput> <!-- refs -->
+    <button class="btn btn-secondary my-2 my-sm-0" type="button" (click)="searchBtnClick(searchInput.value)">Search</button>
+  </form>
   `,
   styles: []
 })
@@ -14,6 +15,10 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  searchBtnClick(value){
+    console.log(value);
   }
 
 }
